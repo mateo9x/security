04b07728c4c447d853a896955bd248025412c198
Mateusz <mtpala@interia.pl>
2023-08-02 17:29:09 +0200

    Initial commit

---
+++ src/main/java/com/mateo9x/security/SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.mateo9x.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+
+}
